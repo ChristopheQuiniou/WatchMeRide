493e4b4447539d1c979a0ce47047484ec6264159
bsessou <bignon-boris-freuxy.sessou@etudiant.univ-rennes1.fr>
2024-06-13 23:57:32 +0200

    Update Card compete

--- src/composants/Card_compete.jsx
+++ src/composants/Card_compete.jsx
@@ -9,7 +9,7 @@ const Card = ({ competition, handleClick }) => {
         <li className=" bg-white border  rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 border-solid border-1 border-gray-600" onClick={() => handleClick(competition.id)}>
             
                 <img 
-                className="rounded-t-lg filter grayscale hover:grayscale-0 min-h-[312px]" 
+                className="rounded-t-lg filter grayscale hover:grayscale-0 h-[312px] " 
                 src={competition.Image}
                 alt={competition.nom} />
             <div className="p-5">
